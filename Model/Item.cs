a19711717577eb3a7bfb637a470553bc6de78da1
Ynychar1980 <yanychar1980@gmail.com>
2021-06-16 12:19:07 +0300

    Добавьте файлы проекта.

---
+++ Model/Item.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LinkedList.Model
+{
+   /// <summary>
+   /// Ячейка списка
+   /// </summary>
+   /// <typeparam name="T"></typeparam>
+   public class Item<T>
+   {
+      private T data = default(T);
+
+      /// <summary>
+      /// Данные хранимые в ячейке списка
+      /// </summary>
+      public T Data
+      {
+         get => data;
+         set
+         {
+            if (value != null)
+            {
+               data = value;
+            }
+            else
+            {
+               throw new ArgumentNullException(nameof(value));
+            }
+
+         }
+      }
+
+      /// <summary>
+      /// Следующая ячейка списка
+      /// </summary>
+      public Item<T> Next { get; set; }
+
+      public Item(T data)
+      {
+         Data = data;
+      }
+
+      public override string ToString()
+      {
+         return Data.ToString();
+      }
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var list = new Model.LinkedList<int>();
+         list.Add(1);
+         list.Add(2);
+         list.Add(3);
+         list.Add(4);
+         list.Add(5);
+
+         foreach (var item in list)
+         {
+            Console.Write(item + " ");
+         }
+         Console.WriteLine();
+
+         list.Delete(3);
+         list.Delete(1);
+         list.Delete(7);
+
+         foreach (var item in list)
+         {
+            Console.Write(item + " ");
+         }
+         Console.WriteLine();
+
+         list.AppendHead(90);
+         list.AppendHead(91);
+         list.AppendHead(92);
+
+         foreach (var item in list)
+         {
+            Console.Write(item + " ");
+         }
+         Console.WriteLine();
+
+         list.InsertAfter(92, 88);
+         list.InsertAfter(5, 77);
+         list.InsertAfter(666, 444);
+         list.InsertAfter(77, 666);
+
+         foreach (var item in list)
+         {
+            Console.Write(item + " ");
+         }
+         Console.WriteLine();
+
+         Console.ReadLine();
+      }
+   }
+}
